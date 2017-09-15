4142865b7401e900ab0e8ffd779b1994eb7ac0cd
stanus <kucharik.s@gmail.com>
2017-09-15 14:51:38 +0200

    models as interfaces

--- src/app/shared/models/ingredient.model.ts
+++ src/app/shared/models/ingredient.model.ts
@@ -1,39 +1,6 @@
-import { Commodity, newCommodityCached } from './commodity.model';
 
-export class Ingredient {
-	private _commodity: Commodity;
-	private _quantity: number;
-	readonly id: number;
-
-	get name (): string {
-		return this._commodity.name;
-	}
-
-	set name (newName: string) {
-		this._commodity.name = newName;
-	}
-
-	get quantity (): number {
-		return this._quantity;
-	}
-
-	set quantity (newQuantity: number) {
-		if (newQuantity > 0) {
-			this._quantity = newQuantity;
-		} else {
-			throw new Error(`Ingredient quantity must be > 0. Your value is ${newQuantity}`);
-		}
-	}
-
-	constructor ({name, quantity, id}: { name: string, quantity: number, id: number }) {
-		this._commodity = newCommodityCached(name);
-		this._quantity = quantity || 1;
-		this.id = id;
-	}
+export interface Ingredient {
+	name: string;
+	quantity: number;
 }
 
-let lastIngredientId = 0;
-
-export function newIngredient ({name, quantity}: { name: string, quantity?: number }): Ingredient {
-	return new Ingredient({name, quantity, id: ++lastIngredientId});
-}
--- src/app/shared/models/recipe.model.ts
+++ src/app/shared/models/recipe.model.ts
@@ -1,71 +1,8 @@
-import { Ingredient, newIngredient } from './ingredient.model';
+import { Ingredient } from './ingredient.model';
 
-export class Recipe {
-	private _ingredients: Array<Ingredient> = [];
-	private _name: string;
-	readonly id: number;
-
-	public description: string;
-	public imagePath: string;
-
-	get name (): string {
-		return this._name;
-	}
-
-	set name (newName: string) {
-		if (newName.length) {
-			this._name = newName;
-		} else {
-			throw new Error(`ingredient name cannot be set to ${newName}`);
-		}
-	}
-
-	get ingredients (): Array<Ingredient> {
-		return this._ingredients;
-	}
-
-	constructor ({name, id, ingredients, description, imagePath}: RecipeConstructorProps) {
-		this._name = name;
-		this.id = id;
-		this.description = description;
-		this.imagePath = imagePath;
-
-		this.addIngredients(ingredients || []);
-	}
-
-	addIngredients (ingredients: RecipeNewIngredients) {
-		const pushIngredient = i => this._ingredients.push(i instanceof Ingredient ? i : newIngredient(i));
-
-		ingredients.forEach(pushIngredient);
-	}
-}
-
-
-let lastRecipeId = 0;
-
-export function newRecipe (newRecipeProps: NewRecipeProps) {
-	if ('id' in newRecipeProps) {
-		return new Recipe(<RecipeConstructorProps>newRecipeProps);
-	}
-	return new Recipe(Object.assign(newRecipeProps, {id: ++lastRecipeId}));
-}
-
-
-interface IngredientSourceData {
+export interface Recipe {
 	name: string;
-	quantity?: number;
-}
-
-interface RecipeConstructorProps extends NewRecipeProps {
-	id: number;
-}
-
-export interface NewRecipeProps {
-	name: string;
-	ingredients?: RecipeNewIngredients;
 	description: string;
 	imagePath: string;
-	id?: number;
+	ingredients: Array<Ingredient>;
 }
-
-type RecipeNewIngredients = Array<IngredientSourceData | Ingredient>;

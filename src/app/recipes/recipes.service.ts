54194d64f3cef9b182abfd8273b585769f5ed2ec
stanus <kucharik.s@gmail.com>
2017-09-15 15:00:07 +0200

    update recipes.service

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { newRecipe, NewRecipeProps, Recipe } from '../shared/models/recipe.model';
+import { Recipe } from '../shared/models/recipe.model';
 import * as _ from 'lodash';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Ingredient } from '../shared/models/ingredient.model';
@@ -11,29 +11,27 @@ export class RecipesService {
 
 	private recipes: Array<Recipe> = [];
 
-	constructor (private shService: ShoppingListService) {
+	constructor (private slService: ShoppingListService) {
 		this.recipes = this.MOCK_getRecipes();
 	}
 
-	addRecipe (recipeData: NewRecipeProps) {
-		this.recipes.push(newRecipe(recipeData));
+	addRecipe (recipe: Recipe) {
+		this.recipes.push(recipe);
 		this.recipesChange.next(_.cloneDeep(this.recipes));
 	}
 
-	updateRecipe (id, recipeData) {
-		const recipePosition = this.recipes.findIndex(r => r.id === id);
-		this.recipes[recipePosition] = newRecipe(recipeData);
+	updateRecipe (index: number, recipe: Recipe) {
+		this.recipes[index] = recipe;
 		this.recipesChange.next(_.cloneDeep(this.recipes));
 	}
 
-	deleteRecipe (id: number) {
-		this.recipes = this.recipes.filter(r => r.id !== id);
+	deleteRecipe (index: number) {
+		this.recipes = this.recipes.splice(index, 1);
 		this.recipesChange.next(_.cloneDeep(this.recipes));
 	}
 
-	getRecipe (id: number): Recipe {
-		const recipe = this.recipes.find(r => r.id === id);
-		return _.cloneDeep(recipe);
+	getRecipe (index: number): Recipe {
+		return _.cloneDeep(this.recipes[index]);
 	}
 
 	getRecipes (): Array<Recipe> {
@@ -41,23 +39,23 @@ export class RecipesService {
 	}
 
 	toShoppingList (ingredients: Array<Ingredient>) {
-		this.shService.addIngredients(ingredients);
+		this.slService.addIngredients(ingredients);
 	}
 
 	private MOCK_getRecipes (): Array<Recipe> {
 		return [
-			newRecipe({
+			{
 				name: 'gule',
 				description: 'gule na vode',
 				ingredients: [{name: 'varlata', quantity: 2}],
 				imagePath: 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Curry_Fish_Balls.jpg'
-			}),
-			newRecipe({
+			},
+			{
 				name: 'vajcia',
 				description: 'ehm',
 				ingredients: [{name: 'bykove varlata', quantity: 6}],
 				imagePath: 'https://upload.wikimedia.org/wikipedia/commons/3/37/Criadillas_de_Choto-_Madrid.jpg'
-			})
+			}
 		];
 	}
 

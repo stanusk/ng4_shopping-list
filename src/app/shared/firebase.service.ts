5b623efa096dc9da473769b6ae974575af37a41b
stano kucharik <kucharik.s@gmail.com>
2017-09-23 11:12:35 +0200

    database operations for authorised users only

--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -3,21 +3,27 @@ import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../auth/auth.service';
 
 // todo rename to db service
 @Injectable()
 export class FirebaseService {
-	private dbUrl = 'https://udemy-course-ng4-7a27a.firebaseio.com/';
+	private dbUrl = 'https://udemy-course-ng4-7a27a.firebaseio.com';
 
 	constructor (
-		private http: Http
+		private http: Http,
+		private auth: AuthService
 	) {}
 
 	save (dataType: string, data: any): Observable<Response> {
-		return this.http.put(this.dbUrl + dataType + '.json', data);
+		const token = this.auth.getToken();
+
+		return this.http.put(`${this.dbUrl}/${dataType}.json?auth=${token}`, data);
 	}
 
 	load (dataType: string): Observable<Response> {
-		return this.http.get(this.dbUrl + dataType + '.json');
+		const token = this.auth.getToken();
+
+		return this.http.get(`${this.dbUrl}/${dataType}.json?auth=${token}`);
 	}
 }

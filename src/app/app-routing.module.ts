f579839923421abc67fd6753c92ee991af8c800e
stano kucharik <kucharik.s@gmail.com>
2017-10-01 21:11:38 +0200

    enable preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/core/components/home/home.component';
 
 const appRoutes: Routes = [
@@ -12,7 +12,7 @@ const appRoutes: Routes = [
 
 @NgModule({
 	imports: [
-		RouterModule.forRoot(appRoutes)
+		RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
 	],
 	exports: [
 		RouterModule

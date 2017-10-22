0b7c4f4ad3e2a25d95bfa313e17d1198abf67334
stano kucharik <kucharik.s@gmail.com>
2017-10-22 22:57:50 +0200

    fix broken refactoring of database service

--- src/app/modules/core/services/database.service.ts
+++ src/app/modules/core/services/database.service.ts
@@ -1,40 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpRequest } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DatabaseService {
 	private dbUrl = 'https://udemy-course-ng4-7a27a.firebaseio.com';
-
-	private _getRequest = this._makeRequestFactory('GET');
-	private _putRequest = this._makeRequestFactory('PUT');
 
 	constructor (
 		private httpClient: HttpClient
 	) {}
 
 	save (dataType: string, data: any): Observable<Object> {
-		return this._putRequest(dataType, data);
+		return this.httpClient.put(`${this.dbUrl}/${dataType}.json`, data);
 	}
-
-	// save (dataType: string, data: any): Observable<Object> {
-	// 	return this.httpClient.put(`${this.dbUrl}/${dataType}.json`, data);
-	// }
 
 	load (dataType: string): Observable<Object> {
-		return this._getRequest(dataType);
-	}
-
-	// load (dataType: string): Observable<Object> {
-	// 	return this.httpClient.get(`${this.dbUrl}/${dataType}.json`);
-	// }
-
-	private _makeRequestFactory (requestType: 'PUT' | 'GET') {
-		return (dataType, data?) => {
-			const url = `${this.dbUrl}/${dataType}.json`;
-			const req = new HttpRequest(requestType, url, data ? data : null);
-
-			return this.httpClient.request(req);
-		};
+		return this.httpClient.get(`${this.dbUrl}/${dataType}.json`);
 	}
 }
